01b4c85f77cb8eafb9ea2d905a129de6d800586b
l4hmm <l4hmm@outlook.com>
2024-12-23 10:03:06 +1100

    *2024-12-23* [Major Changes] - Coded hero section. - Added step 1 and step 2 card stlying with javascript click effects.
    TO DO:
    - Code hero seciton so text and background are responsive with screen sizing.
    - Change icons and images of step 1 and step 2 cards.
    - Continue to edit remaining sections of website.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', function() {
+      // Remove the 'selected' class from all cards
+      document.querySelectorAll('.card').forEach(c => c.classList.remove('selected'));
+      
+      // Add the 'selected' class to the clicked card
+      this.classList.add('selected');
+    });
+  });
